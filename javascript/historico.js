6cbccd8a2919bebd828049b5be31c0d3f58c3387
Joao-Pedro-Al <pv27644@alunos.estgv.ipv.pt>
2025-12-29 10:34:08 +0000

    29-12 - Seleção de meses e datas e adaptar layout para um uso melhor no computador

--- javascript/historico.js
+++ javascript/historico.js
@@ -1,7 +1,16 @@
 const data = document.getElementById('data');
+const MenosAno = document.getElementById('AnoPas');
+const MaisAno = document.getElementById('AnoFut');
+
+const AnoPresente = document.getElementById('data-9');
+const anos = document.querySelectorAll('.ano');
+var anos_inv = new Array;
+const meses = document.querySelectorAll('.mes');
+
 const hoje = new Date;
 var idmes = hoje.getMonth() + 1;
 var mes = "";
+var anoPre = hoje.getFullYear();
 var ano = hoje.getFullYear();
 
 const cartoes = document.querySelectorAll('.card');
@@ -9,10 +18,122 @@ const cartoes = document.querySelectorAll('.card');
 function Start()
 {
     MesString();
-    console.log(idmes + " ---- " + mes);
-    data.textContent = mes + " " + ano;
-    // data.textContent = hoje.toString();
+    AnoPresente.textContent = ano;
+    ListarAnos();
+    // console.log(idmes + " ---- " + mes);
+    AtualizarData();
+    const Mes = document.getElementById("mes-" + idmes);
+    //Ativar Mês Selecionado
+    Mes.classList.toggle("h5");
+    Mes.classList.toggle("p--data");
+    Mes.classList.toggle("h2");
 }
+
+function ListarAnos()
+{
+    const anomax = parseInt(AnoPresente.textContent);
+    var menos = 1;
+    
+    for(var i = 8; i >= 1; i--){
+        const A = document.getElementById("data-" + i);
+        A.textContent = anomax - menos;
+        menos += 1;
+    }
+
+    VerificarAnoSel();
+}
+
+function VerificarAnoSel()
+{
+    anos.forEach(item => {
+        if(item.textContent == ano)
+        {
+            item.classList.toggle('h5');
+            item.classList.toggle('p--data');
+            item.classList.toggle('h2');
+        }
+
+        if(item.classList.contains('h2') && item.textContent != ano)
+        {
+            item.classList.toggle('h5');
+            item.classList.toggle('p--data');
+            item.classList.toggle('h2');
+        }
+    })
+}
+
+MenosAno.addEventListener('click', function() {
+    const anomax = parseInt(AnoPresente.textContent);
+    AnoPresente.textContent = anomax - 1;
+    ListarAnos();
+
+    if(anomax - 1 < anoPre && MaisAno.classList.contains('button--ano--desl'))[MaisAno.classList.toggle('button--ano--desl')]
+})
+
+MaisAno.addEventListener('click', function() {
+    if(MaisAno.classList.contains('button--ano--desl') == false)
+    {
+        const anomax = parseInt(AnoPresente.textContent);
+        AnoPresente.textContent = anomax + 1;
+        ListarAnos();
+
+        if(anomax + 1 == anoPre)[MaisAno.classList.toggle('button--ano--desl')]
+    }
+})
+
+anos.forEach(item => {
+    item.addEventListener('click', function() {
+        if(item.classList.contains("h2") == false)
+        {
+            // //Ativar Mês Selecionado
+            // item.classList.toggle("h5");
+            // item.classList.toggle("p--data");
+            // item.classList.toggle("h2");
+
+            ano = parseInt(item.textContent);
+            VerificarAnoSel();
+            AtualizarData();
+        }
+    })
+});
+
+meses.forEach(item => {
+    item.addEventListener('click', function() {
+        if(item.classList.contains("h2") == false)
+        {
+            console.log("Mês Anterior: " + idmes);
+
+            const mes_ativo = document.getElementById("mes-" + idmes);
+
+            //Revreter Mês Ativo
+            mes_ativo.classList.toggle("h5");
+            mes_ativo.classList.toggle("p--data");
+            mes_ativo.classList.toggle("h2");
+
+            //Ativar Mês Selecionado
+            item.classList.toggle("h5");
+            item.classList.toggle("p--data");
+            item.classList.toggle("h2");
+
+            const Id_String = item.id;
+            idmes = parseInt(Id_String.substring(Id_String.indexOf("-") + 1));
+
+            console.log("Novo Mês: " + idmes);
+
+            AtualizarData();
+        }
+    })
+});
+
+MenosAno.addEventListener('click', function () {
+    console.log("MenosAno");
+    NavegarData(-2);
+});
+
+MaisAno.addEventListener('click', function () {
+    console.log("MaisAno");
+    NavegarData(2);
+});
 
 function MesString()
 {
@@ -55,6 +176,13 @@ function MesString()
             mes = "Dez";
             break;
     }
+}
+
+function AtualizarData(){
+    console.log("Antes Mês String: " + mes);
+    MesString();
+    console.log("Depois Mês String: " + mes);
+    data.textContent = mes + " " + ano;
 }
 
 //Verificar se algum foi clicado
